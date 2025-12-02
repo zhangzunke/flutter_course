ead32dc739895691e2c7ca0384207b1548c9172a
Michael <zhangzunke@126.com>
2025-12-02 22:31:58 +0800

    Code

--- lib/nested_navigation_getx/base_controller.dart
+++ lib/nested_navigation_getx/base_controller.dart
@@ -13,6 +13,7 @@ class BaseController extends GetxController {
       currentIndex.value = newIndex;
     }
     */
+    //
     if (currentIndex.value == newIndex) {
       Get.nestedKey(
         currentIndex,
